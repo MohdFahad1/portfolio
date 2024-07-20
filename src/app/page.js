c3bc479f14f8d3d591eea423fc5bf6a259f48c28
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-07-20 16:18:43 +0530

    feat: Added about me

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div className="flex justify-center p-5 ">
-      <div className="flex flex-col gap-12 md:w-[600px] w-[400px]">
+      <div className="flex flex-col gap-11 md:w-[600px] w-[400px]">
         <Header />
         <About />
       </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,86 @@
+import Image from "next/image";
 import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export const data = [
+  {
+    id: 1,
+    skillName: "HTML",
+    skillLogo: "/images/html.svg",
+  },
+  {
+    id: 2,
+    skillName: "CSS",
+    skillLogo: "/images/css.svg",
+  },
+  {
+    id: 3,
+    skillName: "Javascript",
+    skillLogo: "/images/javascript.svg",
+  },
+  {
+    id: 4,
+    skillName: "Tailwind CSS",
+    skillLogo: "/images/tailwind.svg",
+  },
+  {
+    id: 5,
+    skillName: "React Js",
+    skillLogo: "/images/react.svg",
+  },
+  {
+    id: 6,
+    skillName: "Next Js",
+    skillLogo: "/images/next.svg",
+  },
+  {
+    id: 7,
+    skillName: "MongoDB",
+    skillLogo: "/images/mongodb.svg",
+  },
+];
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mb-2">Hey there 👋,</h1>
+      <p className="font-light">
+        I am <span className="font-semibold">Mohd Fahad</span>, a fullstack
+        developer from <span className="font-semibold">India</span>, crafting
+        things for the internet using the technologies I know and learn. I
+        mostly work with <span className="font-semibold">Javascript</span>{" "}
+        ecosystem and tools.
+      </p>
+      <div className="mt-5">
+        <h1 className="font-bold text-2xl">Tech Stack: </h1>
+        <div className="flex items-center gap-5 mt-3">
+          {data.map((item) => (
+            <TooltipProvider key={item.skillName}>
+              <Tooltip asChild>
+                <TooltipTrigger asChild>
+                  <Image
+                    src={item.skillLogo}
+                    height={35}
+                    width={35}
+                    alt={item.skillName}
+                    className="cursor-pointer"
+                  />
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{item.skillName}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
