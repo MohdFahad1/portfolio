19af676adb904ad52070461b7a528d781826623c
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-07-21 12:31:05 +0530

    fix: apostrophe

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,10 +5,10 @@ const Contact = () => {
     <div>
       <h1 className="font-bold text-2xl mb-2">Get in touch</h1>
       <p className="text-sm ">
-        I'm currently looking for Internship in Software Development or in Web
-        Development.! If you know of any positions available, or if you have any
-        questions, or if you just want to say hi, please feel free to email me
-        at{" "}
+        I&apos;m currently looking for Internship in Software Development or in
+        Web Development.! If you know of any positions available, or if you have
+        any questions, or if you just want to say hi, please feel free to email
+        me at{" "}
         <a
           href="mailto:fahadmohammad312@gmail.com"
           target="_blank"
