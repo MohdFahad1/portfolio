1ab3545618803d3a5f23d30c3834dc5c8682a6b4
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-04-05 18:24:47 +0530

    chore: update career requirements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,12 @@ import React from "react";
 const Contact = () => {
   return (
     <div>
-      <h1 className="font-bold text-2xl mb-2">Get in touch</h1>
+      <h1 className="mb-2 text-2xl font-bold">Get in touch</h1>
       <p className="text-sm ">
-        I&apos;m currently looking for Internship in Software Development or in
-        Web Development! If you know of any positions available, or if you have
-        any questions, or if you just want to say hi, please feel free to email
-        me at{" "}
+        I&apos;m currently looking for full time roles in Software Development
+        or in Web Development! If you know of any positions available, or if you
+        have any questions, or if you just want to say hi, please feel free to
+        email me at{" "}
         <a
           href="mailto:fahadmohammad312@gmail.com"
           target="_blank"
